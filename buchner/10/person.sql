8b8a941b04b06aecb13723e4c7e611b1b375b8fe
Hermine Bschlangaul <hermine.bschlangaul@gmx.net>
2022-11-15 22:30:20 +0100

    Buchner 10

--- buchner/10/person.sql
+++ buchner/10/person.sql
@@ -1,4 +1,4 @@
--- https://www.ccbuchner.de/clip_code-38010-03
+-- https://www.ccbuchner.de/clip_code-38010-07/
 
 CREATE DATABASE  IF NOT EXISTS `sql_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sql_db`;
@@ -20,34 +20,29 @@ USE `sql_db`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Basketball`
+-- Table structure for table `Person`
 --
 
-DROP TABLE IF EXISTS `Basketball`;
+DROP TABLE IF EXISTS `Person`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Basketball` (
-  `Name` text,
+CREATE TABLE `Person` (
+  `Id` int DEFAULT NULL,
   `Vorname` text,
-  `Klasse` text,
-  `Jahre` int DEFAULT NULL,
-  `Größe` double DEFAULT NULL,
-  `1Pkt` int DEFAULT NULL,
-  `2Pkt` int DEFAULT NULL,
-  `3Pkt` int DEFAULT NULL,
-  `R` int DEFAULT NULL,
-  `Block` int DEFAULT NULL
+  `Name` text,
+  `Gehalt` int DEFAULT NULL,
+  `Vorgesetzter` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Basketball`
+-- Dumping data for table `Person`
 --
 
-LOCK TABLES `Basketball` WRITE;
-/*!40000 ALTER TABLE `Basketball` DISABLE KEYS */;
-INSERT INTO `Basketball` VALUES ('Ehrlicher','David','10a',16,182.3,7,14,3,11,9),('Daecher','Christian','10b',15,178.9,5,5,8,2,0),('Kreß','Torben','10a',15,170.8,3,10,2,0,0),('Green','Lothar','10b',16,180,5,9,0,5,4),('Haberland','Konstantin','10b',14,176,3,3,2,2,1),('Haid','Erich','10b',15,175.2,2,7,6,8,1),('Schnitzer','Ulrich','10c',15,177.4,0,1,3,4,2),('Gilmer','Noah','10d',15,170.8,5,6,5,2,0),('Hill','Hannes','10d',15,168.4,4,2,11,3,0),('Panhorst','Hans-Peter','10d',15,170.2,1,4,3,8,3);
-/*!40000 ALTER TABLE `Basketball` ENABLE KEYS */;
+LOCK TABLES `Person` WRITE;
+/*!40000 ALTER TABLE `Person` DISABLE KEYS */;
+INSERT INTO `Person` VALUES (1003,'James ','Khang ',45000,'1008'),(1006,'Christine ','Herzog ',38000,'1008'),(1007,'Erik ','Györf ',36000,'1008'),(1008,'Sos ','Hunor ',60000,'1009'),(1009,'Achim ','Barone ',150000,''),(1010,'Jasmin ','Kaiser ',60000,'1009'),(1012,'David','Goldschmidt',36000,'1022'),(1013,'Wolfgang','Duerr',37000,'1008'),(1014,'Lena','Ebersbacher',38000,'1022'),(1015,'Sophie','Gardiner',38000,'1008'),(1016,'Ivana','Jakubcova',90000,'1009'),(1018,'Miroslava','Kastankova',36000,'1008'),(1019,'Matilda','van den Houten',45000,'1022'),(1020,'Jacoba','Muilenburg',36000,'1027'),(1022,'Bi','Tai',60000,'1009'),(1023,'Bao','Wang',39000,'1027'),(1024,'Gabrjel','Adamski',36000,'1027'),(1026,'Makary','Kwiatkowski',38000,'1027'),(1027,'Leo','De Luca',55000,'1009'),(1029,'Emilia','Siciliani',36000,'1022'),(1031,'Matilde','Bianchi',45000,'1022'),(1033,'Uwe ','Kortig',38000,'1027'),(1035,'Barbara ','Krueger',38000,'1027'),(1036,'Robert ','Naumann',42000,'1008');
+/*!40000 ALTER TABLE `Person` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -59,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-25  7:09:31
+-- Dump completed on 2022-07-25  7:09:37
